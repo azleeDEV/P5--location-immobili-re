5a55bbd73c9a5656a5c62f85d85a331fc3b1d5a6
AZLee <amjedzehani@gmai.com>
2024-07-23 04:04:30 +0200

    finalisation de la page about

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import './NotFound.scss';
+import '../NotFound/NotFound.scss';
+import { Link } from 'react-router-dom';
+import image404 from "../../assets/404.svg"
 
 const NotFound = () => {
     return (
-        <div>NotFound</div>
+        <div>
+            <div className="page-404">
+                <img src={image404} alt='Erreur 404'/>
+                <p>Oups! La page que vous demandez n'existe pas..</p>
+                <Link  to="/">Retourner sur la page d’accueil</Link>
+            </div>
+        </div>
     );
   }
   export default NotFound; 
